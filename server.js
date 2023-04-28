534545db6851f606530b7a79ff6e460aedfac110
Joeykim <joeykim@DESKTOP-3MMF113.localdomain>
2023-04-27 23:31:21 -0400

    Fixed server.js

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ app.get('/app/', (req, res) => {
 
 // rps endpoint returns {"player": "(rock|paper|scissors)""}
 app.get('/app/rps/', (req, res) => {
-    res.status(200).send(JSON.stringify(rps()))
+    res.status(200).send(JSON.stringify(rps_function()))
 });
 
 // rpsls endpoint returns {"player": "(rock|paper|scissors|spock|lizard)"}
 app.get('/app/rpsls/', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls()))
+    res.status(200).send(JSON.stringify(rpsls_function()))
 });
 
 
@@ -44,11 +44,11 @@ app.get('/app/rpsls/', (req, res) => {
 */
 // using URLEncoded query parameters (req.query):
 app.get('/app/rps/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.query.shot)))
+    res.status(200).send(JSON.stringify(rps_function(req.query.shot)))
 });
 // using JSON body requests (req.body):
 app.post('/app/rps/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot)))
+    res.status(200).send(JSON.stringify(rps_function(req.body.shot)))
 })
 
 /*  following routes should return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)",
@@ -56,25 +56,25 @@ app.post('/app/rps/play/', (req, res) => {
 */
 // using URLEncoded query parameters (req.query):
 app.get('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot)))
+    res.status(200).send(JSON.stringify(rpsls_funtion(req.query.shot)))
 });
 // using JSON body requests (req.body):
 app.post('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
+    res.status(200).send(JSON.stringify(rpsls_function(req.body.shot)))
 })
 
 /*  using route parameters (req.params): 
     returns {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
 */
 app.get('/app/rps/play/:shot/', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.shot)))
+    res.status(200).send(JSON.stringify(rps_function(req.params.shot)))
 });
 
 /*  using route parameters (req.params):
     returns {"player":"(rock|paper|scissors|lizard|spock)","opponent":"(rock|paper|scissors|lizard|spock)","result":"(win|lose|tie)"}
 */
 app.get('/app/rpsls/play/:shot/', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
+    res.status(200).send(JSON.stringify(rpsls_function(req.params.shot)))
 });
 
 // default API endpoint that returns 404 NOT FOUND for any endpoints that are not defined
