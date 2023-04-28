04268592075c99752579bbfdb7b98ea02a121adc
Joeykim <joeykim@DESKTOP-3MMF113.localdomain>
2023-04-27 22:47:47 -0400

    Added more functionality with the server

--- server.js
+++ server.js
@@ -27,9 +27,49 @@ app.get('/app/', (req, res) => {
 });
 
 //The check at the end of /app/rps/ that starts that game
-//app.get('/app/rps/',
+app.get('/app/rps/', (req, res) => {
+	res.status(200).send(JSON.stringify(rps()))
+});
 
 //The check at the end of /app/rpsls/ that starts that game
+app.get('/app/rpsls/', (req, res) => {
+	res.status(200).send(JSON.stringify(rpsls()))
+});
 
+//Accepts /app/rps/play/ and /app/rpsls/play respectively. Note that first it deals
+//first with a get and then a post
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)))
+});
+
+app.post('/app/rps/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)))
+})
+
+app.get('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)))
+});
+
+app.post('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
+})
+
+pp.get('/app/rps/play/:shot/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)))
+});
+
+
+app.get('/app/rpsls/play/:shot/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
+});
+
+//Throwing errors in case something goes horribly wrong.
+app.get('*', (req, res) => {
+    res.status(404).send("404 NOT FOUND")
+});
+
+app.listen(port, () => {
+	console.log("Server listening on port " + port);
+})
 
 
